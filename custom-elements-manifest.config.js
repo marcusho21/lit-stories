68d17d0504a00aced1020c713b9e1f0b89be7986
Marcus Ho <42228958+marcusho21@users.noreply.github.com>
2023-01-30 18:13:51 -0500

    update cem outdir path

--- custom-elements-manifest.config.js
+++ custom-elements-manifest.config.js
@@ -2,11 +2,11 @@ import { generateCustomData } from 'cem-plugin-vs-code-custom-data-generator';
 
 export default {
   globs: ['src/components/*.ts'],
-  outdir: 'cem',
+  outdir: 'dist',
   litelement: true,
   plugins: [
     generateCustomData({
-      outdir: 'cem',
+      outdir: 'dist',
       htmlFileName: 'vscode.html-custom-elements.json',
       cssFileName: '',
     }),
